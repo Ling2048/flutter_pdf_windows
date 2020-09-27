de000acae0d14b01533138878aad136847b5e6ec
凌 <L2048@outlook.com>
2020-09-27 19:39:50 +0800

    first commit

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_pdf_windows/flutter_pdf_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterPdfWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterPdfWindowsPlugin"));
+}
